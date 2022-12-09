4be4a40e9adddebaf90ca8ff91879a6eb9639189
Jared Young <104388797+Xanohm710@users.noreply.github.com>
2022-12-09 10:27:41 -0600

    Add AppProvider and hook up to AppBar

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,31 +1,38 @@
 import React from "react";
 import styled, { css } from "styled-components";
+import { AppContext } from "./AppProvider";
 
 const Logo = styled.div`
   font-size: 1.5em;
-`
+`;
 
 const Bar = styled.div`
   display: grid;
   margin-bottom: 40px;
   grid-template-columns: 180px auto 100px 100px;
-`
+`;
 const ControlButtonElem = styled.div`
   cursor: pointer;
-  ${props => props.active && css`
+  ${(props) =>
+    props.active &&
+    css`
       text-shadow: 0px 0px 60px #03ff03;
     `}
-`
+`;
 
-function toProperCase(lower){
-    return lower.charAt(0).toUpperCase() + lower.substr(1);
+function toProperCase(lower) {
+  return lower.charAt(0).toUpperCase() + lower.substr(1);
 }
 
-function ControlButton({name, active}){
+function ControlButton({ name}){
   return (
-    <ControlButtonElem active={active}>
-      {toProperCase(name)}
-    </ControlButtonElem>
+    <AppContext.Consumer>
+      {({ page, setPage }) => (
+        <ControlButtonElem active={page === name} onClick={() => setPage(name)}>
+          {toProperCase(name)}
+        </ControlButtonElem>
+      )}
+    </AppContext.Consumer>
   )
 }
 
@@ -34,9 +41,9 @@ export default function () {
   return (
     <Bar>
       <Logo>CryptoDash</Logo>
-      <div/>
-      <ControlButton active name="dashboard"/>
-      <ControlButton name="settings"/>
+      <div />
+      <ControlButton active name="dashboard" />
+      <ControlButton name="settings" />
     </Bar>
   );
 }
